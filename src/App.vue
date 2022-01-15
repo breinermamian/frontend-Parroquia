999b6fa67c6b408e685c0d4ec9dbface67b6ea1c
breinermamian <breinerm@unicauca.edu.co>
2022-01-15 12:03:37 -0500

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <helloWorld msg="Welcome "/>
+    
+    -->
+    <Register/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import Register from './components/Usuario/Register.vue' 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    Register
   }
 }
 </script>
---
+++ src/components/Usuario/Register.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="Prueba">Saludar</button>
+    <div class="row">
+      <div
+        class="col-xl-4 col-md-6 col-sm-12"
+        v-for="user in allUser.edges"
+        :key="user.node.id"
+      >
+      <h1>{{user.node.name}}</h1>
+      </div>
+    </div> 
+  </div> 
+</template>
+
+<script>
+export default {
+  name: "Register",
+  components: {},
+  data() {
+    return {
+      allUser: Object,
+    };
+  },
+  methods: {
+    Prueba() {
+      console.log("Prueba");
+      console.log("Datos ", this.allUser);
+      console.log("Prueb2a");
+    },
+  },
+  apollo: {
+    allUser: {
+      // Consulta
+      query: require("@/graphql/User/user.gql"),
+      // Asigna el error a la variable definida en data
+      error(error) {
+        this.error = JSON.stringify(error.message);
+      },
+    },
+  },
+};
+</script>
